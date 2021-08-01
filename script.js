32be97f0049fd47309023914fe7259a7bdae3b28
Igor <57453357+misterio-smit@users.noreply.github.com>
2021-08-01 19:02:32 +0700

    выполнил второе задание

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-let money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  period;
+"use strict";
+let money = 55000,
+  income = 'фриланс',
+  addExpenses = 'Бензин, Пиво, Продукты, Коммуналка, Сотовый, Интернет',
+  deposit = true,
+  mission = 300000,
+  period = 6;
 
-console.log("Hello Igor!");
 
-alert("Hello Glo!");
+let lowercase = (addExpenses.toLocaleLowerCase()),
+  splitString = (lowercase.split(', ')),
+  budgetDay = (money / 30);
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log(splitString);
+console.log(budgetDay);
