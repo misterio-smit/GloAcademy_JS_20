080aef1657ec990698cff073a0c193abea5b1839
Igor <57453357+misterio-smit@users.noreply.github.com>
2021-08-17 00:04:36 +0700

    поправил getInfoDeposit

--- script.js
+++ script.js
@@ -104,11 +104,11 @@ let appData = {
   getInfoDeposit: function () {
     if (appData.deposit) {
       do {
-        appData.percentDeposit = prompt('Какой годовой процент?', '10');
+        appData.percentDeposit = +prompt('Какой годовой процент?', '10');
       }
       while (!isNumber(appData.percentDeposit));
       do {
-        appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+        appData.moneyDeposit = +prompt('Какая сумма заложена?', 10000);
       }
       while (!isNumber(appData.moneyDeposit));
 
